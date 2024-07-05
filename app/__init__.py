35aa8fbcd1055d63cc3f2bc8ed92e02438ac386f
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-07-04 20:41:54 -0600

    added image route

--- app/__init__.py
+++ app/__init__.py
@@ -12,10 +12,12 @@ def create_app():
     from .routes.sam_route import sam_bp
     from .routes.moments_route import moment_bp
     from .routes.auth_check_route import auth_check_bp
+    from .routes.images_route import images_bp
     app.register_blueprint(chat_bp)
     app.register_blueprint(sam_bp)
     app.register_blueprint(moment_bp)
     app.register_blueprint(auth_check_bp)
+    app.register_blueprint(images_bp)
 
     socketio.init_app(app)
     return app
--- app/agents/BossAgent.py
+++ app/agents/BossAgent.py
@@ -312,3 +312,22 @@ class BossAgent:
         num_tokens += len(encoding.encode(message))
         num_tokens += 3  # every reply is primed with <|im_start|>assistant<|im_sep|>
         return num_tokens
+    
+    def generate_image(self, request):
+        prompt = request['prompt']
+        size=request['size'].lower()
+        quality=request['quality'].lower()
+        style=request['style'].lower()
+        
+
+        response = self.openai_client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            n=1,
+            size=size,
+            quality=quality,
+            style=style,
+        )
+
+        return response.data[0].url
+    
