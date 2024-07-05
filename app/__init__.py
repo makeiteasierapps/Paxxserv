d492e008f57152d3a678bdc01ff268ac484d8759
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-07-04 19:42:16 -0600

    added auth_check

--- app/__init__.py
+++ app/__init__.py
@@ -11,9 +11,11 @@ def create_app():
     from .routes.chat_route import chat_bp
     from .routes.sam_route import sam_bp
     from .routes.moments_route import moment_bp
+    from .routes.auth_check_route import auth_check_bp
     app.register_blueprint(chat_bp)
     app.register_blueprint(sam_bp)
     app.register_blueprint(moment_bp)
+    app.register_blueprint(auth_check_bp)
 
     socketio.init_app(app)
     return app
