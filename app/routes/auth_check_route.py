4453225e6e726e8f862bae0fa2e4f2f8ef36b934
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-07-07 13:44:21 -0600

    serving firebase config from server

--- app/routes/auth_check_route.py
+++ app/routes/auth_check_route.py
@@ -28,7 +28,7 @@ firebase_service = FirebaseService()
 
 os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
-@auth_check_bp.route('/auth_check', methods=['POST', 'OPTIONS'])
+@auth_check_bp.route('/auth_check', methods=['POST', 'OPTIONS', 'GET'])
 def auth_check():
     """
     Checks if admin has granted access to the user
@@ -36,7 +36,19 @@ def auth_check():
     if request.method == 'OPTIONS':
         return ('', 204)
     
-    uid = request.json.get('uid')
-    user_doc = db['users'].find_one({'_id': uid})  
-    auth_status = user_doc.get('authorized', False)
-    return jsonify({'auth_status': auth_status})
+    if request.method == 'POST':
+        uid = request.json.get('uid')
+        user_doc = db['users'].find_one({'_id': uid})  
+        auth_status = user_doc.get('authorized', False)
+        return jsonify({'auth_status': auth_status})
+    
+    if request.method == 'GET':
+        config = {
+           "apiKey": os.getenv('FIREBASE_API_KEY'),
+           "authDomain": os.getenv('FIREBASE_AUTH_DOMAIN'),
+           "projectId": os.getenv('FIREBASE_PROJECT_ID'),
+           "messagingSenderId": os.getenv('FIREBASE_MESSAGING_SENDER_ID'),
+           "appId": os.getenv('FIREBASE_APP_ID'),
+       }
+        print(config)
+        return jsonify(config)
