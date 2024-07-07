59398abda988d36ee95695ec3fa65cb04268ac48
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-07-07 14:18:52 -0600

    updated run script

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import argparse
 from app import create_app
 from hypercorn.config import Config
 from hypercorn.asyncio import serve
@@ -5,8 +6,20 @@ import asyncio
 
 app = create_app()
 
-if __name__ == '__main__':
+def main():
+    parser = argparse.ArgumentParser(description="Run the server.")
+    parser.add_argument('--mode', choices=['dev', 'prod'], default='prod', help='Run mode: dev or prod')
+    args = parser.parse_args()
+
     config = Config()
     config.bind = ["0.0.0.0:3033"]
-    config.use_reloader = True
+
+    if args.mode == 'dev':
+        config.use_reloader = True
+    else:
+        config.use_reloader = False
+
     asyncio.run(serve(app, config))
+
+if __name__ == '__main__':
+    main()
