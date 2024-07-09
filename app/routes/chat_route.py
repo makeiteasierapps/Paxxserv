a641f3619922c1610ecf17ec88ceb6ff481b5e95
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-07-09 13:02:16 -0600

    had to initialize services in socket event

--- app/routes/chat_route.py
+++ app/routes/chat_route.py
@@ -89,6 +89,21 @@ def chat(subpath):
 
         return ('Chat settings updated', 200)
 
+@chat_bp.route('/messages', defaults={'subpath': ''}, methods=['DELETE', 'POST'])
+@chat_bp.route('/messages/<path:subpath>', methods=['DELETE', 'POST'])
+def handle_delete_all_messages(subpath):
+    if request.method == 'DELETE' and subpath == '':
+        chat_id = request.json.get('chatId')
+        g.chat_service.delete_all_messages(chat_id)
+        return 'Memory Cleared', 200
+    
+    if subpath == 'utils':
+        uid = request.headers.get('userId')
+        file = request.files['image']
+        file_url = g.user_service.upload_file_to_firebase_storage(file, uid)
+        return (json.dumps({'fileUrl': file_url}), 200)
+    
+# SOCKET EVENTS
 @socketio.on('join_room')
 def handle_join_room(data):
     room = data['chatId']
@@ -98,17 +113,19 @@ def handle_join_room(data):
 @socketio.on('chat_request')
 def handle_chat_message(data):
     uid = data.get('userId')
+    db_name = data.get('dbName')
+    chat_service = ChatService(db_name=db_name)
+    user_service = UserService(db_name=db_name)
     system_prompt = None
     chat_settings = data.get('chatSettings', None)
     if chat_settings:
         chat_constants = chat_settings.get('chatConstants')
         use_profile_data = chat_settings.get('useProfileData', None)
         model = chat_settings.get('agentModel', None)
-        print('received model', model)
         system_prompt = chat_settings.get('systemPrompt')
         user_analysis = None
         if use_profile_data:
-            user_analysis = g.user_service.get_user_analysis(uid)
+            user_analysis = user_service.get_user_analysis(uid)
         boss_agent = BossAgent(model=model, chat_constants=chat_constants, system_prompt=system_prompt, user_analysis=user_analysis)
     else:
         boss_agent = BossAgent(model='gpt-4o')
@@ -127,24 +144,10 @@ def handle_chat_message(data):
 
     save_to_db = data.get('saveToDb', True)
     if save_to_db:
-        g.chat_service.create_message(chat_id, 'user', user_message)
+        chat_service.create_message(chat_id, 'user', user_message)
 
     def save_agent_message(chat_id, message):
-        g.chat_service.create_message(chat_id, 'agent', message)
+        chat_service.create_message(chat_id, 'agent', message)
 
     image_url = data.get('imageUrl', None)
     boss_agent.process_message(data['chatHistory'], chat_id, user_message, system_message, save_agent_message if save_to_db else None, image_url)
-
-@chat_bp.route('/messages', defaults={'subpath': ''}, methods=['DELETE', 'POST'])
-@chat_bp.route('/messages/<path:subpath>', methods=['DELETE', 'POST'])
-def handle_delete_all_messages(subpath):
-    if request.method == 'DELETE' and subpath == '':
-        chat_id = request.json.get('chatId')
-        g.chat_service.delete_all_messages(chat_id)
-        return 'Memory Cleared', 200
-    
-    if subpath == 'utils':
-        uid = request.headers.get('userId')
-        file = request.files['image']
-        file_url = g.user_service.upload_file_to_firebase_storage(file, uid)
-        return (json.dumps({'fileUrl': file_url}), 200)
