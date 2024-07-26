e42e1880208050ebe5bce19633a49ebb80172531
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-07-26 13:47:47 -0600

    modified chunkify to handle prechunked data

--- app/agents/DocumentManager.py
+++ app/agents/DocumentManager.py
@@ -11,16 +11,19 @@ class DocumentManager(OpenAiClientBase):
         Tokenizer.initialize()
         self.tokenizer = Tokenizer()
 
-    def chunkify(self, source, content=None, chunks=None):
+    def chunkify(self, source, content=None, highlights=None):
+        if highlights:
+            # Manual chunking
+            return [Document(id=str(uuid.uuid4()), text=highlight['text'], source=source) for highlight in highlights]
+        
+        # No chunking
         if content and self.tokenizer.token_count(content) < 1000:
             return [Document(id=str(uuid.uuid4()), text=content, source=source)]
-        if chunks is None:
-            doc_id = str(uuid.uuid4())
-            chunker = RecursiveCharacterChunker(chunk_size=450)
-            chunks = chunker.chunk_single_document(Document(id=doc_id, text=content, source=source))
-        else:
-            chunks = [Document(id=chunk['id'], text=chunk['text'], source=source) for chunk in chunks]
-        return chunks
+        
+        # Automatic chunking
+        doc_id = str(uuid.uuid4())
+        chunker = RecursiveCharacterChunker(chunk_size=450)
+        return chunker.chunk_single_document(Document(id=doc_id, text=content, source=source))
     
     def embed_chunks(self, chunks):
         encoded_chunks = []
