37319836bdcea379003350404c6ac26f81b698ad
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-08-27 12:03:41 -0600

    refactor

--- app/__init__.py
+++ app/__init__.py
@@ -23,6 +23,12 @@ socketio = SocketIO(cors_allowed_origins="*", async_mode='gevent')
 def create_app():
     app = Flask(__name__)
 
+    CORS(app, resources={r"/*": {
+        "origins": ["https://paxxiumv1.web.app", "http://localhost:3000", "https://shauno.co"],
+        "allow_headers": ["Content-Type", "Accept", "dbName", "uid"],
+        "methods": ["GET", "POST", "OPTIONS", "PUT", "DELETE", "PATCH"],
+    }})
+
     socketio.init_app(app)
 
     if not app.debug:
@@ -41,19 +47,18 @@ def create_app():
     from .routes import chat_route, sam_route, moments_route, auth_check_route, images_route, news_routes, signup_route, profile_route, kb_route
     
     blueprints = [
-        (chat_route.chat_bp, chat_route.cors),
-        (sam_route.sam_bp, sam_route.cors),
-        (moments_route.moment_bp, moments_route.cors),
-        (auth_check_route.auth_check_bp, auth_check_route.cors),
-        (images_route.images_bp, images_route.cors),
-        (profile_route.profile_bp, profile_route.cors),
-        (news_routes.news_bp, news_routes.cors),
-        (signup_route.signup_bp, signup_route.cors),
-        (kb_route.kb_bp, kb_route.cors)
+        chat_route.chat_bp,
+        sam_route.sam_bp,
+        moments_route.moment_bp,
+        auth_check_route.auth_check_bp,
+        images_route.images_bp,
+        profile_route.profile_bp,
+        news_routes.news_bp,
+        signup_route.signup_bp,
+        kb_route.kb_bp
     ]
     
-    for blueprint, cors in blueprints:
-        cors.init_app(blueprint)
+    for blueprint in blueprints:
         app.register_blueprint(blueprint)
 
     from .routes import socket
