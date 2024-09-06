b5470e3ceb6a31e44b92960c4c1e5b12de20177c
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-09-06 17:33:57 -0600

    updated avatar to be stored locally

--- app/services/ProfileService.py
+++ app/services/ProfileService.py
@@ -11,7 +11,7 @@ class ProfileService:
         
         return None
     def get_profile(self, uid):
-        user_doc = self.db['users'].find_one({'_id': uid}, {'first_name': 1, 'last_name': 1, 'username': 1, 'avatar_url': 1, 'analysis': 1})
+        user_doc = self.db['users'].find_one({'_id': uid}, {'first_name': 1, 'last_name': 1, 'username': 1, 'avatar_path': 1, 'analysis': 1})
         
         if user_doc:
             user_doc.pop('_id')
